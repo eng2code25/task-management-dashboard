27593d61492e9116bfa0461a024d3aee4ff23150
Chee Kit <145242543+eng2code25@users.noreply.github.com>
2026-05-21 22:47:52 +0800

    Update App.jsx
    Added drop down action when +Add task is clicked, set prevent default to the save and clear button to prevent the browser from refreshing as it's in a form.

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,53 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [taskInput, setTaskInput] = useState("True");
+  const [editorInput, setEditorInput] = useState("");
+
   return (
     <section>
       <h2>Task Management Dashboard</h2>
       <div>
-        <button>+Add Tasks</button>
+        <button onClick={() => setTaskInput(!taskInput)}>+Add Tasks</button>
       </div>
       <form>
-        <div className="task-editor-container">
-          <div className="task-editor">
-            <textarea
-              placeholder="Write task here..."
-              className="editor"
-            ></textarea>
+        {taskInput && (
+          <div className="task-editor-container">
+            <div className="task-editor">
+              <textarea
+                placeholder="Write task here..."
+                className="editor"
+              ></textarea>
+            </div>
+            <div className="task-condition">
+              <div className="task-status">
+                <select>
+                  <option>Not complete</option>
+                  <option>In progress</option>
+                  <option>Complete</option>
+                </select>
+              </div>
+              <div className="task-priority">
+                <select>
+                  <option>Urgent</option>
+                  <option>Not Urgent</option>
+                </select>
+              </div>
+              <div className="task-due">
+                <input type="datetime-local"></input>
+              </div>
+            </div>
+            <div className="task-action">
+              <div>
+                <button onClick={(e) => e.preventDefault()}>Save</button>
+              </div>
+              <div>
+                <button onClick={(e) => e.preventDefault()}>Clear</button>
+              </div>
+            </div>
           </div>
-          <div className="task-condition">
-            <div className="task-status">
-              <select>
-                <option>Not complete</option>
-                <option>In progress</option>
-                <option>Complete</option>
-              </select>
-            </div>
-            <div className="task-priority">
-              <select>
-                <option>Urgent</option>
-                <option>Not Urgent</option>
-              </select>
-            </div>
-            <div className="task-due">
-              <input type="datetime-local"></input>
-            </div>
-          </div>
-          <div className="task-action">
-            <div>
-              <button>Save</button>
-            </div>
-            <div>
-              <button>Clear</button>
-            </div>
-          </div>
-        </div>
+        )}
       </form>
       <section className="task-summary">
         <h3>Task Summary</h3>
